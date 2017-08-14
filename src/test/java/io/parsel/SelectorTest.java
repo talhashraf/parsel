11c6939027619ad1d08831e6cec1df0e13c51382
Talha Ashraf <talhashraf@gmail.com>
2017-08-14 20:34:47 +0500

    remove unnecessary comments

--- src/test/java/io/parsel/SelectorTest.java
+++ src/test/java/io/parsel/SelectorTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.parsel;
 
 import org.junit.Test;
@@ -15,7 +10,7 @@ import org.w3c.dom.NodeList;
  * Selector class tests.
  * 
  * 
- * @author talha
+ * @author Talha Ashraf
  */
 
 
